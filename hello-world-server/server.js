b45b7a8e44528038d602dd69a6e6a3f034533ea1
samirm00 <sammoham.be@gmail.com>
2024-07-06 18:05:14 +0200

    add basic-crud operations

--- hello-world-server/server.js
+++ hello-world-server/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import { createServer } from 'http';
+import dotenv from 'dotenv';
+
+// Configuring dotenv
+dotenv.config();
+const PORT = process.env.PORT || 5004;
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World\n');
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
